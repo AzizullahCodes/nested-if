34df03cb8c76f6ea71f1443c2c97df3d2fe7a840
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-16 21:19:02 +0500

    u

--- ap.js
+++ ap.js
@@ -52,6 +52,7 @@
 
                 // else {console.log('the number is odd');}
 
+
                 // ================ basic-practice-project-07(Eligibility for online exam))===============
               //               var question = prompt('Have you pay exam fee?(yes or no)');
               //               var question_2 = prompt('Do you have a valid ID card?(yes or no)');
@@ -61,6 +62,8 @@
               // if (question_3 === 'yes'){var flag_3 = question_3;}
               // if(flag_1 && flag_2 && flag_3){console.log('you are eligible for online exam');}
               // else{console.log('you are not eligible for online exam');} 
+
+
 // ================ basic-practice-project-07(Driving test eligibility))===============
               // var question_1 = prompt('Are you 18 years old or older?(yes/no)');
               // var question_2 = prompt('Do you have an Id card?(yes/no)');
@@ -68,17 +71,76 @@
               // if (question_2 === 'yes'){var flag_2 = question_2;}
               // if (flag_1 && flag_2){console.log('you can take the driving test');}
               // else{console.log('you are not eligible');}
-              // ================ basic-practice-project-07(Door lock system))===============
+
+
+              // ================ basic-practice-project-08(Door lock system))===============
                      // var question_1 = prompt('Is the correct password?(yes/no)');
                      // var question_2 = prompt('Is the door closed?(yes/no)');
                      // if (question_1 === 'yes'){var flag_1 = question_1;}
                      // if (question_2 === 'yes'){var flag_2 = question_2;}
                      // if (flag_1 && flag_2) {console.log('The door is now locked');}
                      // else{console.log('Locking failed');}
-                     //  ================ basic-practice-project-08(Discount eligibility checker))===============
-                            var question_1 = prompt('Are you a member?(yes/no)');
-                            var question_2 = prompt('Do you have a coupn?(yes/no)'); 
-                            if (question_1 === 'yes'){var good_1 = question_1;}; 
-                            if(question_2 === 'yes'){var good_2  = question_2;};
-                            if (good_1 || good_2){console.log('you can get a discount');}
-                            else{console.log('No discount available');}
+
+
+                     //  ================ basic-practice-project-09(Discount eligibility checker))===============
+                            // var question_1 = prompt('Are you a member?(yes/no)');
+                            // var question_2 = prompt('Do you have a coupn?(yes/no)'); 
+                            // if (question_1 === 'yes'){var good_1 = question_1;}; 
+                            // if(question_2 === 'yes'){var good_2  = question_2;};
+                            // if (good_1 || good_2){console.log('you can get a discount');}
+                            // else{console.log('No discount available');}
+
+
+                             //  ================ basic-practice-project-10(Age verification for movie))===============
+                                          // var age = prompt('Enter your age');
+                                          // if (age >= 18){console.log('You can watch the movie');}
+                                          // else{console.log('you are not allowed to watch the movie');}
+
+                                          //  ================ basic-practice-project-11(Exam Grade Checker))===============
+                            //               var marks = prompt('Enter your marks(0 to 100)');
+                            //               if (marks >= 50){if(marks >= 80){console.log('you got a grade');}
+                            //        else if (marks >= 65){console.log('You got B grade');}
+                            // else{console.log('you passed with c grade');}}
+
+                            //               else{console.log('you failed');}
+
+                            //  ================ basic-practice-project-12(ATM PIN and Balance))===============
+                            // var pin = +prompt("Enter you pin(use '1234' as correct pin)");
+                            // if (pin === 1234){var money = prompt('Ask the user how much money they want to withdraw');
+                            //        if(money <= 5000){console.log('Transaction successful');}
+                            //        else{console.log('Insufficient Balance');}
+                            // }
+
+                            // else{console.log('Invalid pin');}
+
+                            //  ================ basic-practice-project-13(Traffic light checker))===============
+                            //  var light_color = prompt('Enter traffic light color(yellow,red or green)');
+                            // if (light_color === 'red'){var hurry = prompt('You are in a hurry(yes or no)');
+                            //        if(hurry === 'yes'){console.log('Wait safety first');}
+                            //        else{console.log('Good. Wait for green light');}
+
+
+                            // }
+                            // else if (light_color === 'yellow'){console.log('Get ready to move');}
+                            // else if (light_color === 'green'){console.log('You can go now');}
+                            // else{console.log('Invalid light color');}
+
+
+//  ================ basic-practice-project-14(Student Library Access))===============
+              // var question_1 = prompt('Do you have a student card(yes/no)');
+              // if (question_1 === 'yes'){var validity = prompt('Is your card valid(yes/no)');
+              //        if (validity === 'yes'){console.log('You may enter the library');}
+              //        else{console.log('Your card is expired');}
+              // }
+              // else if (question_1 === 'no'){console.log('You need a student card to entery');}
+              // else{console.log('Invalid entry');}
+
+
+              //  ================ basic-practice-project-15(Weather Clothes suggestion)===============
+              // var question_1 = prompt('Ask it is raining?(yes/no)');
+              // if (question_1 === 'yes'){var umbrella = prompt('Do you have an umbrella?(yes/no)');
+              //        if (umbrella === 'yes'){console.log('Take your umbrella before going out');}
+              //        if(umbrella === 'no'){console.log('Wait until the rain stop');}   
+              // }
+              //           else if(question_1 === 'no'){console.log('enjoy your day');}
+              //           else{console.log('Invalid entry');}                         
