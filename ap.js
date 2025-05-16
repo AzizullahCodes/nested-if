61486976e30579750e29b836b9270f38671da657
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-16 17:11:58 +0500

    upda

--- ap.js
+++ ap.js
@@ -1,12 +1,53 @@
 // in this assignment my project are related to nested if practice
 // ================ basic-practice-project-01(Number sign checker)===============
-var number = +prompt('Enter a number');
-if (number > 0){if( number > 100){console.log('Big positive number')}
-else{console.log('small positive number')}
-}
+// var number = +prompt('Enter a number');
+// if (number > 0){if( number > 100){console.log('Big positive number')}
+// else{console.log('small positive number')}
+// }
+// else if(number === 0) {console.log('Number is zero');}
+// else{console.log('number is negative');}
+
+// ================ basic-practice-project-02(Simple login system)===============
+// var userName = prompt('Enter user name');
+// if (userName === 'admin'){ var password = +prompt('Enter password');
+//     if (password === 1234){console.log('Login successful');}
+//     else{console.log('wrong password');}
+// }
+// else{console.log('user not found')} 
+// ================ basic-practice-project-03(voting eligibility)===============
+//  var age = prompt('Enter your age');
+//  if (age >= 18) {var id = prompt('the user has an id card,yes or no');
+//     if(id === 'yes'){console.log('you can vote');}
+//     else{console.log('you need a voter ID to vote');}
+
+//  }
+ 
+//  else{console.log('you are not eligible for voting');}
+ // ================ basic-practice-project-04(Simple traffic light checker))===============
+//  var  trafficLightColor = prompt('Enter traffic light color,red,yellow or green')
+//  if (trafficLightColor === 'red'){console.log('stop');}
+//  else if (trafficLightColor === 'yellow'){var moving = prompt('Are you already moving,(yes or no)');
+//     if(moving === 'yes'){console.log('slow down');}
+//     else{console.log('wait');}
+//  }
+//  else if (trafficLightColor === 'green'){console.log('go');}
+//  else{console.log('enter proper command or content');}
 
 
+ // ================ basic-practice-project-05(simple calculator))===============
+        // var number1 = +prompt('Enter first number');
+        // var number2 = +prompt('Enter second number');
+        // var user_will = prompt('you will add both numbers(yes or no)');
+        // if (user_will === 'yes') {if(number1 > 0 , number2 > 0){console.log(number1 + number2);}
+        // else{console.log('only positive numbers are allowed');}}
+        // else if (user_will === 'no'){console.log('operation cancelled');}
+        // else{console.log('only enter suitable comman');}
 
+         // ================ basic-practice-project-06(Simple traffic light checker))===============
+                // var number = prompt('Enter a number');
+                // var checker = number % 2;
+                // if (checker === 0){if (number > 100){console.log('The number is even and large');}
+                // else{console.log('even and small number')}}
+                
 
-else if(number === 0) {console.log('Number is zero');}
-else{console.log('number is negative');}
+                // else {console.log('the number is odd');}
