ef669b7625c71e5516280659f196cb8741cc0a93
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-16 12:24:50 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,12 @@
+// in this assignment my project are related to nested if practice
+// ================ basic-practice-project-01(Number sign checker)===============
+var number = +prompt('Enter a number');
+if (number > 0){if( number > 100){console.log('Big positive number')}
+else{console.log('small positive number')}
+}
+
+
+
+
+else if(number === 0) {console.log('Number is zero');}
+else{console.log('number is negative');}
